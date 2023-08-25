f1ef611f41e1f52d38675d0f7e1251c8feeeea60
tanmaypakhale <tanmaypakhale@gmail.com>
2023-08-26 04:03:20 +0530

    file added

--- discord-frontend/src/shared/AlertNotification.js
+++ discord-frontend/src/shared/AlertNotification.js
@@ -7,9 +7,9 @@ import { getActions } from '../store/actions/alertActions';
 const AlertNotification = ({showAlertMessage,closeAlertMessage,alertMessageContent}) => {
   return (
     <Snackbar anchorOrigin={ {vertical: 'bottom', horizontal: 'center'}}
-    open
-    onClose={()=>{}}
-    autoHideDuration={6000}
+      open={showAlertMessage}
+      onClose={closeAlertMessage}
+      autoHideDuration={6000}
     >
         <Alert severity='info'>{alertMessageContent}</Alert>
     </Snackbar>
